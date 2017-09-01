d9a663cad00503de836bf9a08691323ff7daf47a
Caleb <caleb@calebthompson.com>
2017-08-31 20:25:31 -0700

    ooof

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,7 @@ import java.util.logging.Logger;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-
-import com.sun.jersey.spi.container.servlet.ServletContainer;
+import org.glassfish.jersey.servlet.ServletContainer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
